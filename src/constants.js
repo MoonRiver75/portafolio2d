a30c0cbb439cb2e143f44d53bdbb8f65fb9dcc58
MoonRiver75 <124457379+MoonRiver75@users.noreply.github.com>
2024-04-11 15:15:56 -0700

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+export const dialogueData = {
+    pc: `Este es mi PC. Trabajo principalmente en C# y Python en estos dias. He hecho un par de proyectos en ese lenguaje. Tambien me gusta el desarrollo de paginas web. De todos modos, independientemente del lenguaje, simplemente me gusta programar`,
+    "cs-degree": `Este premio del INJUVE es un logro del que me siento profundamente orgulloso. Sin embargo, lo que mas felicidad me brinda es poder compartirlo con personas de un talento excepcional.`,
+    "sofa-table": 'Este es mi sofa y me encanta para poder descansar despues de quemarme las neuronas programando o haciendo lo que mas disfruto, despues de clases siendo maestro y estudiante dependiendo del dia.',
+    tumba: 'Este es un recuerdo de todos los anelos que se quedaron rotos en el camino.',
+    tv: `Esa es mi television en la que suelo ver videos acerca de <a href="https://youtu.be/DQnua5LEDMw?si=s0lZ3dS08VoT1rZ4" target="_blank">Linux</a> la distro de Hanna Montanna la mejor de todas.`,
+    bed: `Bienvenido a mi cama, no tengo ganas de dormir `,
+    resume: `Este es mi CV <a href="https://drive.google.com/drive/folders/1fHHRtv3ZNGy9do0OYGd3F_64NnG58lLk?usp=sharing" target="_blank">Puedes revisarlo</a>
+    contactame roldanmadero@gmail.com si tienes alguna vacante!`,
+    projects: `Informacion sobre este portafolio: Esta hecho con la biblioteca Kaboom.js, la cual es una biblioteca para crear juegos en JavaScript. El texto se renderiza con HTML/CSS. Por lo tanto, el cuadro de texto que estas leyendo actualmente no se renderiza dentro del lienzo (canvas)`,
+    library: `Mi libreria alberga una amplia variedad de lecturas, desde clasicos como 'Mujercitas' y 'Los Miserables', hasta una extensa seleccion de literatura. Ademas, libros especializados en programacion.`,
+    impac:'He tenido un par de certificaciones en distintos momentos puedes checar mi <a href="https://www.linkedin.com/in/roldan-madero/" target="_blank">Linkedin</a>',
+    uy:'Pronto se haran mas cositas por aqui ........',
+    vini: 'Ella es Vinitra Heart la draga mas famosa de Tepic, ya esta descansando y nunca va a volver a presentarse pero la quiero mucho <3 ',
+    exit: `Que te vaya bien hermoso ser.`,
+    
+  };
