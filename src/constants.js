81958f17d158a56e6e16df63356ae13d7a9fc31d
MoonRiver75 <124457379+MoonRiver75@users.noreply.github.com>
2024-04-11 23:26:50 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -7,13 +7,16 @@ export const dialogueData = {
     tv: `Esa es mi television en la que suelo ver videos acerca de <a href="https://youtu.be/DQnua5LEDMw?si=s0lZ3dS08VoT1rZ4" target="_blank">Linux</a> la distro de Hanna Montanna la mejor de todas.`,
     bed: `Bienvenido a mi cama, no tengo ganas de dormir `,
     resume: `Este es mi CV <a href="https://drive.google.com/drive/folders/1fHHRtv3ZNGy9do0OYGd3F_64NnG58lLk?usp=sharing" target="_blank">Puedes revisarlo</a>
-    contactame roldanmadero@gmail.com si tienes alguna vacante!`,
+    contactame roldanmadero16@gmail.com si tienes alguna vacante!`,
     projects: `Informacion sobre este portafolio: Esta hecho con la biblioteca Kaboom.js, la cual es una biblioteca para crear juegos en JavaScript. El texto se renderiza con HTML/CSS. Por lo tanto, el cuadro de texto que estas leyendo actualmente no se renderiza dentro del lienzo (canvas)`,
     library: `Mi libreria alberga una amplia variedad de lecturas, desde clasicos como 'Mujercitas' y 'Los Miserables', hasta una extensa seleccion de literatura. Ademas, libros especializados en programacion.`,
     impac:'He tenido un par de certificaciones en distintos momentos puedes checar mi <a href="https://www.linkedin.com/in/roldan-madero/" target="_blank">Linkedin</a>',
     uy:'Pronto se haran mas cositas por aqui ........',
     vini: 'Ella es Vinitra Heart la draga mas famosa de Tepic, ya esta descansando y nunca va a volver a presentarse pero la quiero mucho <3 ',
-    refri:'Bienvenido a mi fantasia para conocer mas sobre tu personas favorita Roldan Madero, tomate una kombucha, relajate y espero que disfrutes <3',
+    refri:'Bienvenido a mi fantasia para conocer mas sobre tu persona favorita, Roldan Madero, tomate una kombucha, relajate y espero que disfrutes <3',
+    rois:'Este es mi perrito Roise, es tierno <3',
+    pinky:'Esta es mi perrita Pinky, no vamos a molestarla ',
+    arbol:'Una manzana para el camino',
     exit: `Que te vaya bien hermoso ser.`,
     
   };
